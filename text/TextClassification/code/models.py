3eafb1208dcd7a84a47d64c08492688da99a2547
Hao Luo <1091627587@qq.com>
2022-08-25 22:34:19 +0800

    Update models.py

--- text/TextClassification/code/models.py
+++ text/TextClassification/code/models.py
@@ -1,13 +1,21 @@
 from torch import nn
 
-
 class TextClassificationModel(nn.Module):
 
     def __init__(self, vocab_size, embed_dim, num_class):
         super(TextClassificationModel, self).__init__()
+        self.num_layers = 2
         self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
-        self.fc = nn.Linear(embed_dim, num_class)
+#         self.lstm = nn.LSTM(embed_dim, 64, self.num_layers, bidirectional=True)
+        self.gru = nn.GRU(embed_dim, 64, self.num_layers, bidirectional=True)
+        self.fc = nn.Linear(128, num_class)
         self.init_weights()
+#         # lstm parameters
+#         self.h0 = torch.randn(2*self.num_layers, barch_size, 128)
+#         self.c0 = torch.randn(2*self.num_layers, barch_size, 128)
+#         # gru parameters
+#         self.h1 = torch.randn(2*self.num_layers, barch_size, 256)
+#         self.c1 = torch.randn(2*self.num_layers, barch_size, 256)
 
     def init_weights(self):
         init_range = 0.5
@@ -16,11 +24,15 @@ class TextClassificationModel(nn.Module):
         self.fc.bias.data.zero_()
 
     def forward(self, text, offsets):
-        embedded = self.embedding(text, offsets)
-        return self.fc(embedded)
+        x = self.embedding(text, offsets)
+#         x = self.lstm(x, (self.h0,self.c0))
+#         x = self.gru(x, (self.h1,self.c1))
+#         x,(_h,_c) = self.lstm(x)
+        x, _h = self.gru(x)
+        x = self.fc(x)
+        return x
 
 
 if __name__ == '__main__':
-    model = TextClassificationModel(100, 100, 10)
+    model = TextClassificationModel(100, 100, 4)
     print(model)
-
