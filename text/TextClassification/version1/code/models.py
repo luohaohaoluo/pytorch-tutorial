88a3dff6ebe82cc18feb8f76484a4bec149e77e5
Hao Luo <1091627587@qq.com>
2022-08-29 16:19:00 +0800

    Update models.py

--- text/TextClassification/version1/code/models.py
+++ text/TextClassification/version1/code/models.py
@@ -22,32 +22,6 @@ class TextClassificationModel(nn.Module):
         x = self.fc(x)
         return x
 
-#
-# class TextClassificationModel(nn.Module):
-#
-#     def __init__(self, vocab_size, embed_dim, num_class):
-#         super(TextClassificationModel, self).__init__()
-#         self.embedding = nn.Embedding(vocab_size, embed_dim)
-#         self.conv1d = nn.Conv1d(embed_dim, 128, kernel_size=3, stride=2)
-#         self.lstm = nn.LSTM(128, 256, batch_first=True)
-#         self.fc = nn.Linear(256, num_class)
-#
-#     def forward(self, x):
-#         x = self.embedding(x)
-#         # print("embedding:", x.shape)
-#
-#         x = x.permute(0, 2, 1)
-#         x = self.conv1d(x)
-#         # print("conv1d:", x.shape)
-#
-#         x = x.permute(0, 2, 1)
-#         x, (_h, _c) = self.lstm(x)
-#         # print("lstm:", x.shape)
-#
-#         x = self.fc(x[:, -1, :])
-#         # print("fc:", x.shape)
-#         return x
-
 
 if __name__ == '__main__':
     model = TextClassificationModel(100, 100, 4)
