185fd1e0707e8f37f875a4b2b5727adfa427d931
Hao Luo <1091627587@qq.com>
2022-08-29 16:33:32 +0800

    Update models.py

--- text/TextClassification/version2/code/models.py
+++ text/TextClassification/version2/code/models.py
@@ -1,28 +1,5 @@
 from torch import nn
 
-
-# class TextClassificationModel(nn.Module):
-#
-#     def __init__(self, vocab_size, embed_dim, num_class):
-#         super(TextClassificationModel, self).__init__()
-#         self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
-# #         self.lstm = nn.LSTM(embed_dim, 64, self.num_layers, bidirectional=True)
-# #         self.gru = nn.GRU(embed_dim, 64, self.num_layers, bidirectional=True)
-#         self.fc = nn.Linear(embed_dim, num_class)
-#         self.init_weights()
-#
-#     def init_weights(self):
-#         init_range = 0.5
-#         self.embedding.weight.data.uniform_(-init_range, init_range)
-#         self.fc.weight.data.uniform_(-init_range, init_range)
-#         self.fc.bias.data.zero_()
-#
-#     def forward(self, text, offsets):
-#         x = self.embedding(text, offsets)
-#         x = self.fc(x)
-#         return x
-
-
 class TextClassificationModel(nn.Module):
 
     def __init__(self, vocab_size, embed_dim, num_class):
